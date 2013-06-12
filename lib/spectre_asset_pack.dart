7263e618bc5e26120d03f201345d39484c16084d
Kevin Moore <kevin@thinkpixellab.com>
2013-06-11 22:20:41 -0400

    fixes for latest SDK

--- lib/spectre_asset_pack.dart
+++ lib/spectre_asset_pack.dart
@@ -27,7 +27,6 @@ import 'dart:html';
 import 'dart:json' as JSON;
 import 'dart:math' as Math;
 import 'dart:async';
-import 'dart:uri';
 import 'dart:typed_data';
 import 'package:asset_pack/asset_pack.dart';
 import 'package:spectre/spectre.dart';
