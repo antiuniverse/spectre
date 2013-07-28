1f79ae1aef10e98d8f89698e528bd3b363dc70f4
John McCutchan <john@johnmccutchan.com>
2013-07-28 10:46:09 -0700

    Work towards supporting all constants.
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- lib/src/spectre_declarative/material_constant.dart
+++ lib/src/spectre_declarative/material_constant.dart
@@ -28,23 +28,24 @@ import 'package:spectre/spectre_declarative_main.dart';
 import 'package:spectre/spectre_element.dart';
 import 'package:vector_math/vector_math.dart';
 
-/*
-<s-material-constant name="sourceR" texture="assetpack://asset.pack.blah" minification="mipmap">
-</s-material-constant>
-<s-material-constant name="cameraView" data="[]">
-</s-material-constant>
-<s-material-constant name="blend.source" value="">
-</s-material-constant>
-<s-material-constant name="depth.func" value="">
-</s-material-constant-uniform>
-*/
-
 class SpectreMaterialConstantElement extends SpectreElement {
   String name;
+
+  // State constants
+  dynamic value;
+
+  // Texture constants.
+  bool _isSampler = false;
   SamplerState _sampler;
   SpectreTexture _texture;
-  dynamic index;
-  bool _isSampler = false;
+  dynamic _index;
+
+  SamplerState get sampler => _sampler;
+  SpectreTexture get texture => _texture;
+
+  // Uniform constants.
+  bool _isUniform = false;
+  // _index
 
   created() {
     super.created();
@@ -78,14 +79,16 @@ class SpectreMaterialConstantElement extends SpectreElement {
   apply() {
     super.apply();
     _update();
-    var scene = DeclarativeState.scene;
-    var graphicsContext = DeclarativeState.graphicsContext;
-    if (index == null) {
-      return;
-    }
     if (_isSampler) {
-      graphicsContext.setTexture(index, _texture);
-      graphicsContext.setSampler(index, _sampler);
+      _applySampler();
+    } else if (_isUniform) {
+      _applyUniform();
+    } else if (_isRasterizerConstant(name)) {
+      _applyRasterizerConstant(name);
+    } else if (_isDepthConstant(name)) {
+      _applyDepthConstant(name);
+    } else if (_isBlendConstant(name)) {
+      _applyBlendConstant(name);
     }
   }
 
@@ -106,19 +109,190 @@ class SpectreMaterialConstantElement extends SpectreElement {
     if (name == null) {
       return;
     }
-    var graphicsDevice = DeclarativeState.graphicsDevice;
     var currentMaterial = DeclarativeState.scene.currentMaterial;
     if (currentMaterial == null) {
       return;
     }
-    var sampler = currentMaterial.shaderProgram.samplers[name];
-    _isSampler = sampler != null;
-    if (_isSampler) {
-      index = sampler.textureUnit;
-      _texture = DeclarativeState.getAsset(attributes['texture']);
-      // TODO(johnmccutchan): Update sampler attributes.
+    if (_isRasterizerConstant(name)) {
+      _updateRasterizerConstant(name);
+    } else if (_isDepthConstant(name)) {
+      _updateDepthConstant(name);
+    } else if (_isBlendConstant(name)) {
+      _updateBlendConstant(name);
     } else {
-      index = sampler.location;
+      _index = null;
+      var sampler = currentMaterial.shaderProgram.samplers[name];
+      _isSampler = sampler != null;
+      if (sampler != null) {
+        _updateSampler(sampler);
+      } else {
+        var uniform = currentMaterial.shaderProgram.uniforms[name];
+        _isUniform = uniform != null;
+        if (uniform != null) _updateUniform(uniform);
+      }
     }
+  }
+
+  void _applySampler() {
+    if (_index == null) {
+      return;
+    }
+    assert(_isSampler);
+    var graphicsContext = DeclarativeState.graphicsContext;
+    graphicsContext.setTexture(_index, _texture);
+    graphicsContext.setSampler(_index, _sampler);
+  }
+
+  void _updateSampler(ShaderProgramSampler sampler) {
+    _index = sampler.textureUnit;
+    _texture = DeclarativeState.getAsset(attributes['texture']);
+  }
+
+  void _applyUniform() {
+  }
+
+  void _updateUniform(ShaderProgramUniform uniform) {
+  }
+
+  void _applyRasterizerConstant(String name) {
+    assert(inited);
+    assert(_isRasterizerConstant(name));
+  }
+
+  void _updateRasterizerConstant(String name) {
+    assert(inited);
+    assert(_isRasterizerConstant(name));
+    var currentMaterial = DeclarativeState.scene.currentMaterial;
+    if (currentMaterial == null) {
+      return;
+    }
+    switch (name) {
+      case 'cullMode':
+        break;
+      case 'frontFace':
+        break;
+      case 'depthBias':
+        break;
+      case 'slopeScaleDepthBias':
+        break;
+      case 'scissorTestEnabled':
+        break;
+    }
+  }
+
+  void _applyDepthConstant(String name) {
+    assert(inited);
+    assert(_isRasterizerConstant(name));
+  }
+
+  void _updateDepthConstant(String name) {
+    assert(inited);
+    assert(_isDepthConstant(name));
+    var currentMaterial = DeclarativeState.scene.currentMaterial;
+    if (currentMaterial == null) {
+      return;
+    }
+    switch (name) {
+      case 'depthBufferEnabled':
+        break;
+      case 'depthBufferWriteEnabled':
+        break;
+      case 'depthBufferFunction':
+        break;
+    }
+  }
+
+  void _applyBlendConstant(String name) {
+    assert(inited);
+    assert(_isRasterizerConstant(name));
+  }
+
+  void _updateBlendConstant(String name) {
+    assert(inited);
+    assert(_isBlendConstant(name));
+    var currentMaterial = DeclarativeState.scene.currentMaterial;
+    if (currentMaterial == null) {
+      return;
+    }
+    switch (name) {
+      case 'enabled':
+        break;
+      case 'blendFactorRed':
+        break;
+      case 'blendFactorGreen':
+        break;
+      case 'blendFactorBlue':
+        break;
+      case 'blendFactorAlpha':
+        break;
+      case 'alphaBlendOperation':
+        break;
+      case 'alphaDestination':
+        break;
+      case 'alphaSourceBlend':
+        break;
+      case 'colorBlendOperation':
+        break;
+      case 'colorDestinationBlend':
+        break;
+      case 'colorSourceBlend':
+        break;
+      case 'writeRenderTargetRed':
+        break;
+      case 'writeRenderTargetGreen':
+        break;
+      case 'writeRenderTargetBlue':
+        break;
+      case 'writeRenderTargetAlpha':
+        break;
+    }
+  }
+
+  static bool _isRasterizerConstant(String name) {
+    List<String> rasterizerConstants = ['cullMode', 'frontFace', 'depthBias',
+                                        'slopeScaleDepthBias',
+                                        'scissorTestEnabled'];
+    for (var i = 0; i < rasterizerConstants.length; i++) {
+      if (name == rasterizerConstants[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static bool _isDepthConstant(String name) {
+    List<String> depthConstants = ['depthBufferEnabled',
+                                   'depthBufferWriteEnabled',
+                                   'depthBufferFunction'];
+    for (var i = 0; i < depthConstants.length; i++) {
+      if (name == depthConstants[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static bool _isBlendConstant(String name) {
+    List<String> blendConstant = ['enabled',
+                                  'blendFactorRed',
+                                  'blendFactorGreen',
+                                  'blendFactorBlue',
+                                  'blendFactorAlpha',
+                                  'alphaBlendOperation',
+                                  'alphaDestination',
+                                  'alphaSourceBlend',
+                                  'colorBlendOperation',
+                                  'colorDestinationBlend',
+                                  'colorSourceBlend',
+                                  'writeRenderTargetRed',
+                                  'writeRenderTargetGreen',
+                                  'writeRenderTargetBlue',
+                                  'writeRenderTargetAlpha'];
+    for (var i = 0; i < blendConstant.length; i++) {
+      if (name == blendConstant[i]) {
+        return true;
+      }
+    }
+    return false;
   }
 }
