8b2eaa71f95258569025b10adc97ca448483e19f
John McCutchan <john@johnmccutchan.com>
2013-08-11 12:01:08 -0700

    Don't automatically call _generateMipmap
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- lib/src/spectre/texture_2d.dart
+++ lib/src/spectre/texture_2d.dart
@@ -56,7 +56,6 @@ class Texture2D extends SpectreTexture {
     _width = width;
     _height = height;
     _uploadPixelArray(width, height, array);
-    _generateMipmap();
     device.context.setTexture(device.context._tempTextureUnit, old);
   }
 
@@ -90,7 +89,6 @@ class Texture2D extends SpectreTexture {
       device.gl.texImage2DVideo(_textureTarget, 0, pixelFormat, pixelFormat,
                                 pixelDataType, element);
     }
-    _generateMipmap();
     device.context.setTexture(device.context._tempTextureUnit, old);
   }
 
