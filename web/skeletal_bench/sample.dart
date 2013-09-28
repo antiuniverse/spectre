c1a995bc1d69cf4187601d85cdc9366225a73d4c
John Evans <prujohn@gmail.com>
2013-09-28 12:08:19 -0500

    re issue #124. added export for vector math in spectre.dart. removed all unneeded references.  updated readme.

--- web/skeletal_bench/sample.dart
+++ web/skeletal_bench/sample.dart
@@ -28,7 +28,6 @@ import 'dart:html';
 import 'dart:math' as Math;
 import 'dart:async';
 import 'dart:typed_data';
-import 'package:vector_math/vector_math.dart';
 import 'package:game_loop/game_loop_html.dart';
 import 'package:asset_pack/asset_pack.dart';
 import 'package:spectre/spectre.dart';
